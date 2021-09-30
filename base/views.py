08b83cd9fd4a4302ba74bfca1cc5974942249f63
Shameel Fazul <shameel.fazul@mail.com>
2021-09-30 18:44:51 +0530

    setup route params, template engine and dynamic routes

---
+++ base/views.py
@@ -0,0 +1,26 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Lets learn C++'},
+    {'id': 3, 'name': 'Lets learn C#'}
+]
+
+def home(request):
+    context = { 'rooms': rooms }
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+              room = i
+              break
+          
+    context = { 'room': room }
+         
+    return render(request, 'base/room.html', context)
+    
